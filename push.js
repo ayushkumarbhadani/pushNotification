625b66f27f6d3c0416d38763145ed30f9694bb69
Ayush Kumar Bhadani <ayushkumarbhadani@gmail.com>
2022-08-20 18:52:56 +0530

    fix

--- push.js
+++ push.js
@@ -10,5 +10,14 @@ let vapidKeys = {
 
 push.setVapidDetails("mailto:ayushkumarbhadani@gmail.com", vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = { "endpoint": "https://fcm.googleapis.com/fcm/send/dFnwDumwqMQ:APA91bHSrR_ihCvnXzdzXpo76vn442mAi0EoKozs1m58hTVDerGK2yk5BSaX08EZmC4T66rREzmhFY0flC5080dmKg1t8elZ-JTbnv6k2RQVBAtuTU_t10DIpNK5E2LE5A51bsMp5So5", "expirationTime": null, "keys": { "p256dh": "BN8NB28HQ2AHs24xoOIyvtVBa0ANvmoLmH503VGF5dq7B5GlDJVey1zlWTMhRxTmiO81oS1CXFSnqQS6MSZkJsk", "auth": "YrtVY4DUrQILBf7iaOmMRQ" } }
-push / push.sendNotification(sub, 'test message');
+let sub = {
+    endpoint:
+        "https://fcm.googleapis.com/fcm/send/dFnwDumwqMQ:APA91bHSrR_ihCvnXzdzXpo76vn442mAi0EoKozs1m58hTVDerGK2yk5BSaX08EZmC4T66rREzmhFY0flC5080dmKg1t8elZ-JTbnv6k2RQVBAtuTU_t10DIpNK5E2LE5A51bsMp5So5",
+    expirationTime: null,
+    keys:
+    {
+        p256dh: "BN8NB28HQ2AHs24xoOIyvtVBa0ANvmoLmH503VGF5dq7B5GlDJVey1zlWTMhRxTmiO81oS1CXFSnqQS6MSZkJsk",
+        auth: "YrtVY4DUrQILBf7iaOmMRQ"
+    }
+}
+push.sendNotification(sub, 'test message');
